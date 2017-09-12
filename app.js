46c158796413c85ca9ffaf4ae0266f6c2132a46b
George Embry <georgetembry@yahoo.com>
2017-09-12 12:54:11 -0700

    added app.js index.html style.css .eslintrc .[gitignore]

---
+++ app.js
@@ -0,0 +1,12 @@
+'USE STRICT';
+
+var projects = [];
+function MyProject(name, date, description) {
+this.name = name;
+this.date = date;
+this.description = description;
+
+    projects.push(myproject);
+
+}
+new MyProjects = ("Salmon cookies", "May2017", "designed app for mock business");
