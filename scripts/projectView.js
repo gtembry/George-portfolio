bbcfa81e250a667c3e33e71bd195c6514089c611
George Embry <georgetembry@yahoo.com>
2017-09-25 12:27:17 -0700

    changed var to let

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var projects = [];
+let projects = [];
 
 
 function Project(rawObj) {
@@ -10,16 +10,16 @@ function Project(rawObj) {
 
 // need a function that will render the object's data into HTML for one project at a time. This function should probably just belong to the Project prototype.
 
-var rawTemplateHTML = $('#projectTemplate').html();
-var actualTemplateFunction = Handlebars.compile(rawTemplateHTML);
+let rawTemplateHTML = $('#projectTemplate').html();
+let actualTemplateFunction = Handlebars.compile(rawTemplateHTML);
 
 Project.prototype.georgeWritesToHTML= function(idx){
   // fill template with this object's data
-  var theHTML = actualTemplateFunction(this);
+  let theHTML = actualTemplateFunction(this);
   // append the newly-written html to the DOM
   // if the project's index is a multiple of 3, make a new row
   if (!(idx % 3)) {
-    var newRow = $('<div class="row"></div>');
+    let newRow = $('<div class="row"></div>');
     $('main').append(newRow);
     newRow.append(theHTML);
   } else {
@@ -28,6 +28,6 @@ Project.prototype.georgeWritesToHTML= function(idx){
 }
 
 rawData.forEach(function(oneProject, idx){
-  var proj = new Project(oneProject);
+  let proj = new Project(oneProject);
   proj.georgeWritesToHTML(idx);
 });
